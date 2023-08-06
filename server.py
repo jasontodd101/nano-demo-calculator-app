801414ba0519aac30b23aa4aa9f96ca2dcf82f81
jasontodd101 <yashkagi007@gmail.com>
2023-08-06 11:29:39 +0530

    made changes

--- server.py
+++ server.py
@@ -13,21 +13,21 @@ app = Flask(__name__)
 @app.route("/calculator/greeting", methods=['GET'])
 def greeting():
     return 'Hello world!'
-    return ''
+    # return ''
 
 @app.route("/calculator/add", methods=['POST'])
 def add():
     numbers = request.json
     response = Result(numbers['first'] + numbers['second'])
     return jsonify(response)
-    return ''
+    # return ''
 
 @app.route("/calculator/subtract", methods=['POST'])
 def subtract():
     numbers = request.json
     response = Result(numbers['first'] - numbers['second'])
     return jsonify(response)
-    return ''
+    # return ''
 
 if __name__ == '__main__':
     app.run(port=8000,host='0.0.0.0')
